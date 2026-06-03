7c739bd1076305f839c983f51044a400a4079498
Your Name <you@example.com>
2026-06-03 10:52:54 +0530

    Implemented Logging Middleware

---
+++ logging_middleware/RequestLoggingFilter.java
@@ -0,0 +1,35 @@
+package logging_middleware;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        long startTime = System.currentTimeMillis();
+
+        System.out.println("Incoming Request:");
+        System.out.println("Method: " + request.getMethod());
+        System.out.println("URI: " + request.getRequestURI());
+
+        filterChain.doFilter(request, response);
+
+        long endTime = System.currentTimeMillis();
+
+        logger.info("Response Status: " + response.getStatus());
+        logger.info("Time Taken: " + (endTime - startTime) + " ms");
+    }
+}
